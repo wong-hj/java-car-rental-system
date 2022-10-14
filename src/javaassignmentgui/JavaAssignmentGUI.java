ae7315100ac7d36fab4e470bdef458fe4dd0efb1
june <hj.wong02@gmail.com>
2022-10-14 15:11:32 +0800

    Created Login Page

--- src/javaassignmentgui/JavaAssignmentGUI.java
+++ src/javaassignmentgui/JavaAssignmentGUI.java
@@ -5,7 +5,7 @@ public class JavaAssignmentGUI {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello world 1");
+        
     }
     
 }
