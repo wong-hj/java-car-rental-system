624a459ac707ec192c073a35314b20b77b7d216f
Kaito <bryan021212@gmail.com>
2022-10-23 14:57:52 +0800

    register, not sure what changed in login form

--- src/javaassignmentgui/Customer.java
+++ src/javaassignmentgui/Customer.java
@@ -4,10 +4,20 @@ package javaassignmentgui;
 public class Customer {
     private String username;
     private String password;
+    private String gender;
+    private String age;
+    private String phoneNum;
+    private String email;
+    private String address;
 
-    public Customer(String username, String password) {
+    public Customer(String username, String password, String gender, String age, String phoneNum, String email, String address) {
         this.username = username;
         this.password = password;
+        this.gender = gender;
+        this.age = age;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.address = address;
     }
 
     public String getUsername() {
@@ -25,6 +35,47 @@ public class Customer {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
     
     
+  
 }
--- src/javaassignmentgui/DataIO.java
+++ src/javaassignmentgui/DataIO.java
@@ -1,9 +1,13 @@
 
 package javaassignmentgui;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 
 public class DataIO {
@@ -30,5 +34,53 @@ public class DataIO {
         }
         return login = false;
         
+    }
+    
+    private static boolean CheckExistingUser(String fileName, String username) throws FileNotFoundException {
+        boolean userExist = false;
+        
+        Scanner s = new Scanner(new File(fileName));
+        
+        while(s.hasNext()) {
+            
+            
+            String line = s.nextLine().trim();
+            
+            String [] dataRow = line.split("\\|");
+            
+            if(username.equals(dataRow[0])) {
+                JOptionPane.showMessageDialog(null, "User already exist!", "Error!", JOptionPane.ERROR_MESSAGE);
+                userExist = true;
+            }
+        
+        }
+        
+        return userExist;
+    }
+    
+    public static boolean RegisterUser(String fileName, String username, String password, String gender, String age, String phoneNum, String email, String address) throws FileNotFoundException {
+        boolean register = false;
+        Customer cust = new Customer(username, password, gender, age, phoneNum, email, address);
+        boolean userExist = CheckExistingUser(fileName, username);
+        
+        if (userExist == false) {
+            File oldFile = new File(fileName);
+            
+            String FullNewUserLine = username + "|" + password + "|" + gender + "|" + age + "|" + phoneNum + "|" + email + "|" + address + "\n";
+            String FullNewUserLine1 = cust.getUsername() + "|" + cust.getPassword() + "|" + cust.getGender() + "|" + cust.getAge() + "|" + cust.getPhoneNum() + "|" + cust.getEmail() + "|" + cust.getAddress() + "\n";
+            
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter(oldFile, true));
+                bw.append(FullNewUserLine);
+                bw.close();
+                
+                register = true;
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            
+        }
+        
+        return register;
     }
 }
